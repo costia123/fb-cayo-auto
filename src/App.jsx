89779f1038ae422a3bca73bb3b1b850a678dfc25
costia123 <jardincostia@gmail.com>
2023-05-12 18:46:22 +0200

    fix template

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
 import React from "react";
-import { Routes ,Route } from 'react-router-dom';
+import { Switch, BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "screens/Home";
 
 function App() {
-	
-	return (
-		<>
+  return (
+    <>
+      <Router>
 		<Routes>
-				<Route path="/" element={<Home/>}/>
-
+          <Route path="/" element={<Home/>}/>
 		</Routes>
-		</>
-	);
+      </Router>
+    </>
+  );
 }
-
 export default App;
